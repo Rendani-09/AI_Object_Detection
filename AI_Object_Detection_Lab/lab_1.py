b10ba8c3f94ada50d882743ff185a0980740a818
Rendani Muthambi <166339016+Rendani-09@users.noreply.github.com>
2026-03-05 07:42:37 +0200

    Refactor comments for clarity in lab_1.py

--- AI_Object_Detection_Lab/lab_1.py
+++ AI_Object_Detection_Lab/lab_1.py
@@ -2,13 +2,13 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")  # downloads model on first run
 
-source = r"Asian market.jpg"  # change to image, video, or 0 for webcam
+source = r"Asian market.jpg"  # for image file
 
-#Remove the comment from the line below to test with a video file and webcam instead of an image
+#Remove the comment(#) from the lines below to test with a video file and webcam instead of an image
 
-#source = r"Cars Moving On Road Footage.mp4"  # change to image, video, or 0 for webcam
+#source = r"Cars Moving On Road Footage.mp4"  # for video file
 
-source = 0  # change to image, video, or 0 for webcam
+#source = 0  # for webcam
 
 if isinstance(source, str) and source.lower().endswith((".jpg", ".jpeg", ".png", ".bmp")):
     model.predict(source=source, conf=0.35, show=True, save=True)
@@ -16,3 +16,4 @@ else:
     model.track(source=source, tracker="bytetrack.yaml", conf=0.35, show=True, save=True)
 
 print("Done. Check runs/ folder.")
+
